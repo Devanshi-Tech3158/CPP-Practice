c19814ec65ef805fab0f34ead7e40d35074b37fb
Devanshi-Tech3158 <devanshi3158@gmail.com>
2026-07-06 19:48:30 +0530

    Day 1: C++ Basics Programs
    Added basic C++ practice programs.

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int l,b;
+
+    cout<<"enter l:";
+    cin>>l;
+
+    cout<<"enter b:";
+    cin>>b;
+
+    int mul= l*b;
+    cout<<"area of rectangle="<<mul<<endl;
+
+    return 0;
+
+}
